7b5458b041f20f49cba9973c1a2d08aef4a20858
Abby Heiser <aheiser2@yahoo.com>
2022-08-29 20:59:10 -0500

    add new grid button with functionality, and new color button to change color

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,6 @@
 // function to create the number of needed square divs in the grid
 let viewerNum = 0
+let currentColor = "pink"
 
 function askForNumber(){
     let answer = parseInt(prompt("How many squares per side?"))
@@ -18,6 +19,7 @@ function askForNumber(){
 console.log(viewerNum)
 
 function gridCreator(number){
+    askForNumber()
     number = viewerNum
     for(let i=1; i <= (number*number); i++){
         let div = document.createElement('div')
@@ -31,21 +33,36 @@ function gridCreator(number){
         document.getElementById("gridContainer").appendChild(div)
     }
 }
-askForNumber()
-console.log(viewerNum)
+// askForNumber()
+// console.log(viewerNum)
 
 gridCreator()
 
 
 //function to change grid div color when it's passed over (hover)
 function hover(event){
-    event.target.style.backgroundColor = "black"
+    event.target.style.backgroundColor = `${currentColor}`
 }
 
 
 // function that connects to button in HTML that asks user for the 
 // number of squares they want along side of grid (set limit to 100)
 
+const btn = document.querySelector('#btn')
+btn.addEventListener('click', () => {
+    let divsToRemove = document.getElementsByTagName('div')
+    for (let i = divsToRemove.length-1; i>= 0; i--){
+        divsToRemove[i].remove()
+    }
+    gridCreator()
+})
+
 
 // extra credit: see if you can make colors change (either on purpose
 // or at random), or get darker if passed over multiple times
+
+const btn2 = document.querySelector('#btn2')
+btn2.addEventListener('click', () => {
+    let answer = prompt("What color do you want?")
+    currentColor = answer
+})
